eb6d0b4b7b083052f90f287bc3bc94d622789cc3
igueyetraining <ismaelgueyetraining@gmail.com>
2025-04-27 16:05:12 -0400

    Udpate version to 0.0.2-SNAPSHOT

--- src/main/java/com/hk/prj/userbook/controller/HelloController.java
+++ src/main/java/com/hk/prj/userbook/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.hk.prj.userbook.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class HelloController {
+	
+	@Autowired
+	BuildProperties buildProperties;
 
     @GetMapping("/hello")
     public String hello(){
 		log.info("hello requested.... ");
-        return "Hello World Userbook App";
+        return "Hello World [" + buildProperties.getName() + ":" + buildProperties.getVersion() + "]";
     }
 }
